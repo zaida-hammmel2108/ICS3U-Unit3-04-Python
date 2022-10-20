1bffdb04b61905eade189e071b05623df7ada265
Zaida Hammel <zzaida.hammel@mths.ca>
2022-10-20 14:35:53 +0000

    added python files

---
+++ integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This program identifies your integer
+
+def main():
+    # This function checks if the number you chose matches the random number.
+
+    # input
+    user_number = int(input("Enter any integer: "))
+
+    # process
+        # process
+    if user_number < 0:
+        # output
+        print("{0} is a negative integer!".format(user_number))
+    elif user_number > 0:
+        # output
+        print("{0} is a positive integer!.".format(user_number))
+    else:
+        # output
+        print("{0} is just zero!.".format(user_number))
+
+    print("")
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
